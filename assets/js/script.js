59e1235a2953d5773457699f7e8e409c62ec69ea
DESKTOP-V4M5IG8\QuyLAPTOP <ledinhquy280799@gmail.com>
2021-10-28 09:45:12 +0700

    init project

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#map').click(function() {
+        $('#map').toggleClass('showMap');
+        $('#openMap').toggle();
+        $('#closeMap').toggle();
+    })
+});

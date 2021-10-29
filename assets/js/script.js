8fffd9b5320f110b36c9439121bf6d3ed2c0a7a8
DESKTOP-V4M5IG8\QuyLAPTOP <ledinhquy280799@gmail.com>
2021-10-29 10:26:21 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,4 +33,49 @@ $(document).ready(function() {
         $(".togglesearch").toggle();
         $("input[type='text']").focus();
     });
+
+    // var lastScrollTop = 0;
+    // navbar = document.getElementById('navigation-bar');
+    // window.addEventListener('scroll', function() {
+    //     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    //     if (scrollTop > lastScrollTop) {
+    //         navbar.style.top = "-70px";
+    //     } else {
+    //         navbar.style.position = "fixed";
+    //         navbar.style.top = "0";
+    //         navbar.style.left = "0";
+    //         navbar.style.right = "0";
+    //         navbar.style.background = "#f28989";
+    //     }
+    //     lastScrollTop = scrollTop;
+    // })
+
+    // var scroll = $(document).scrollTop();
+    // var navHeight = $('#navigation-bar').outerHeight();
+
+    // $(window).scroll (function() {
+    //     var scrolled = $(document).scrollTop();
+
+    //     if (scrolled > navHeight) {
+    //         $('#navigation-bar').addClass('animate')
+    //     } else {
+    //         $('#navigation-bar').removeClass('animate')
+    //     }
+
+    //     if (scrolled > scroll) {
+    //         $('#navigation-bar').removeClass('sticky')
+    //     } else {
+    //         $('#navigation-bar').addClass('sticky')
+    //     }
+
+    //     scroll = $(document).scrollTop();
+    // });
+    $(window).scroll(function() {
+        console.log($(document).scrollTop());
+        if ($(document).scrollTop() > 50) {
+            $("#navigation-bar").addClass('sticky');
+        } else {
+            $("#navigation-bar").removeClass('sticky');
+        }
+    });
 });
